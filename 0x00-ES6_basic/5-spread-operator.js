edd60e6fd72f66c99fc816047613c58acd36252b
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-07-22 20:57:34 +0000

    task 5

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,3 +1,6 @@
-export default function concatArrays(array1, array2, string) { 
+/*export default function concatArrays(array1, array2, string) { 
   return array1.concat(array2).concat(string.split(''));
+}*/
+export default function concatArrays(array1, array2, string) {
+  return [...array1, ...array2, ...string];
 }
