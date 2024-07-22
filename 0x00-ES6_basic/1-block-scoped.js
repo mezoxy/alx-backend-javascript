316af116f97debbab494b53af98b2ced037aae01
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-07-22 20:23:11 +0000

    task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  task;
+  task2;
 
   if (trueOrFalse) {
-    const task;
-    const task2;
+   var task = false;
+   var task2 = true;
   }
 
   return [task, task2];
