dbdd79749479dbaa86aa090fc0b2fa177a82b324
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-07-24 21:12:30 +0000

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,6 +3,10 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map((res) => ({status : res.status, value: res.status === 'fulfilled' ? res.value  : String(res.reason)})
-));
+    .then((results) => results.map(
+      (res) => ({
+        status: res.status,
+        value: res.status === 'fulfilled' ? res.value : String(res.reason),
+      }),
+    ));
 }
