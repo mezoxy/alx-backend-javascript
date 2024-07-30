6ada347b988258779e65d6d5283a4098d415001e
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-07-30 15:26:58 +0100

    task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,4 +1,4 @@
 export default function updateStudentGrandeByCity(lis, str, grades) {
-  lis.map((o) => (return {...o, grade = grades.filter((i) => i.studentId === o.id).length === 0 ? 'N/A' : grades.filter((i) => i.studentId === o.id)[0].grade}));
+  lis.map((o) => return o.grade = grades.filter((i) => i.studentId === o.id).length === 0 ? 'N/A' : grades.filter((i) => i.studentId === o.id)[0].grade);
   return lis.filter((o) => o.location === str);
 }
