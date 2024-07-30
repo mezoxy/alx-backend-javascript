05b3835ed2d69fc053481de860fc05dafd8334fe
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-07-30 19:36:55 +0100

    Task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,5 @@
 export default function cleanSet(set, start) {
-  const lis = []
-  let str = ''
-  set.forEach((i) => i.starsWith(start) && start.length !== 0 ? lis.push(i.substr(start.length)) : lis.push(''));
-  lis.forEach((i) => i.length !== 0 ? str += i + '-' : str += '');
+  let str = '';
+  set.forEach((i) => i.startsWith(start) && start.length !== 0 ? str += i + '-' : str += '');
   return str.length !== 0 ? str.substr(0, str.length -1) : str;
 }
