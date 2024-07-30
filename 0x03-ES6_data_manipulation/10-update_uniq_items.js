614a7fb55dd2b8a1582938c6c2b4c64656f88d98
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-07-30 22:26:23 +0100

    Task 10

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,12 @@
+export default function updateUniqueItems(mapedItem) {
+  try {
+    mapedItem.forEach((i, j) => {
+      if (i === 1) {
+      mapedItem.set(j, 100);
+      }
+    });
+  }
+  catch(err) {
+    throw err;
+  }
+}
