cc68af0501755a0b1535b84c5254ab032f4811fa
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-09-17 17:52:32 +0100

    Task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 process.stdin.setEncoding('utf-8');
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (input) => {
-	console.log(`Your name is ${input}`);
+	console.log(`Your name is ${input.slice(0, -1)}`);
 	if (!process.stdin.isTTY) {
 		console.log('This important software is now closing');
 	}
