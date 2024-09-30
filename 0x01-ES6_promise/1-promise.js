513e0cf5e7b153eab55c0a7f7915ee09522e7c58
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-09-30 21:01:54 +0100

    Task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,6 @@
 export default function getFullResponseFromAPI(success) {
-    if (success === true) {
-      return Promise.resolve({ status: 200, body: 'Success' });
-    } else {
-      return Promise.reject(new Error('The fake API is not working currently'));
-    }
+  if (success === true) {
+    return Promise.resolve({ status: 200, body: 'Success' });
+  }
+  return Promise.reject(new Error('The fake API is not working currently'));
 }
