9b8ebfe20077890679a8fbab4c1428b9ebe257f6
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-09-30 22:30:49 +0100

    Task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => 
-    return {
-    {
-      status: 200,
-      body: 'success'
-    };
-  }).catch(()=> {
-    return new Error();
-  }).finally(() => {
-     console.log('Got the response from the API')};
-)}
+    .then(() => ({status: 200, body: 'success'}))
+    .catch(()=> new Error())
+    .finally(() => console.log('Got the response from the API'));
+}
