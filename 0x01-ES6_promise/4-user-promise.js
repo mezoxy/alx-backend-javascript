13e2beda3ae6fb4882f154cefe44004001e3a08f
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-10-01 12:58:07 +0100

    Task 4

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,4 +1,3 @@
 export default function signUpUser(firstName, lastName) {
-  return Promise
-    .resolve((firstName, lastName) => ({ 'firstName': firstName, 'lastName': lastName }));
+  return Promise.resolve({ 'firstName': firstName, 'lastName': lastName });
 }
