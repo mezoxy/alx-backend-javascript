856c96f700c1d54882dd4197f8122276679d33fb
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-10-01 12:32:22 +0100

    Task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,4 +3,5 @@ import { uploadPhoto, createUser } from './utils'
 export default function handleProfileSignup(){
  Promise.all([uploadPhoto, createUser])
     .then((res) => console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`))
-    .catch((err) => console.log('body firstName lastName'))
+    .catch((err) => console.log('Signup system offline'))
+}
